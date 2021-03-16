d41b7969a6350b3fa5c68d033fa8c1e3ae23236f
ManuelStaffa <46997457+ManuelStaffa@users.noreply.github.com>
2021-03-16 21:07:18 +0100

    initial again

---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = "Nzk1MzEzODA5MDczODk3NTA0.X_HjtA.STP6rFjbbzt3AiQaYsvaxBl-aKI"
+REDDIT_KEY = "boyeOaSoCZhJ2qFZlYTGh62vVZnabg"
+REDDIT_CLIENT_ID = "wacXQcgEpbOqNg"
+
+
+#cloud = heroku.from_pass('hentei.dev@gmail.com', '$hentEi_423%')
